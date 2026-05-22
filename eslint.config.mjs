8dd7b1eb711cf2665d4735b4355413b1a9fe0ff7
jvrb <j.vgms2@hotmail.com>
2026-05-22 20:32:06 -0300

    fix: corrige configuração flat config do ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,16 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  js.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
+];
